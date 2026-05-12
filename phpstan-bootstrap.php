acfab2a0967b61ef0ad88a76f0d857754869ca09
Bryan <bryan@vuii.co.uk>
2026-05-12 15:39:36 +0000

    perf(fixAggregates): covering index + JOIN/GROUP BY + chunked bulk UPDATE

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Schema\Blueprint;
 
 // Register Blueprint macros so Larastan's MacroMethodsClassReflectionExtension
 // can resolve them via the runtime $macros static property.
-Blueprint::macro('nestedSet', function (): void {});
-Blueprint::macro('dropNestedSet', function (): void {});
+Blueprint::macro('nestedSet', function (string|array $scope = [], string|array $cover = []): void {});
+Blueprint::macro('dropNestedSet', function (string|array $scope = [], string|array $cover = []): void {});
 Blueprint::macro('nestedSetAggregate', function (string $column, string $type = 'sum_count'): void {});
 Blueprint::macro('dropNestedSetAggregate', function (string $column): void {});
